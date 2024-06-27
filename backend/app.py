eb004ad07c93f6daff096abeac78a9e52fbed4d6
LuceilIsRight <lucedeshmukh@gmail.com>
2024-06-27 16:11:54 +0000

    Add sign-up and login functionality using Flask

---
+++ backend/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+# In-memory user storage
+users = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users:
+            return 'User already exists!'
+        users[username] = password
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username not in users or users[username] != password:
+            return 'Invalid credentials!'
+        session['username'] = username
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
