d3f84c3b8698a43f836df31a41acf7a7d6650b1a
LuceilIsRight <lucedeshmukh@gmail.com>
2024-06-27 16:17:30 +0000

    Update routes to render HTML templates using Flask

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,14 @@ users = {}
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
